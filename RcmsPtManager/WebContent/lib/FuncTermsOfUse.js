1edcc8871cd9709dc33c8ddfa940cba2e2f51b3c
sonpro <isonpro@gmail.com>
2015-01-27 21:29:43 +0900

    rebase new1

--- RcmsPtManager/WebContent/lib/FuncTermsOfUse.js
+++ RcmsPtManager/WebContent/lib/FuncTermsOfUse.js
@@ -1,10 +1,19 @@
-var prjRuleName	=	"RcmsUsageRules";
+var projectRuleName	=	"RcmsUsageRules";
 
-var RcmsValueToName = RcmsValueToName ||(function(){
+
+/*
+ * Description
+ * RcmsValueToNameDic에 등록된 key value 를 번역하여 준다
+ * 
+ * */
+var TranslaterKeyValue = TranslaterKeyValue ||(function(){
 
 	var RcmsValueName 	=	"";
 	
 	return{
+		/*
+		 * key에 대응하는 error message 또는 name 을 반환
+		 */
 		getValuetoName : function( dv, val ){
 			RcmsValueName	=	eval("RcmsValueToNameDic."+dv);
 			for(var idx in RcmsValueName){
@@ -20,6 +29,13 @@ var RcmsValueToName = RcmsValueToName ||(function(){
 	}
 })(this)
 
+
+/*
+ * browser 단에서의 데이터 검증,가공 등을 담당한다
+ * 
+ * functions
+ * 1) chkFormValues : 규칙코드에 따른 필수값체크
+ */
 var RcmsPTManager = RcmsPTManager || (function() {
     var frm = document.frm;
     //var data = "secret";
@@ -27,7 +43,7 @@ var RcmsPTManager = RcmsPTManager || (function() {
     return {
     	
       /*
-       * 필수값체크
+       * 규칙코드에 따른 필수값체크
        * param
        * 	frmName 
        * 	pValue	: 규칙코드 RcmsUsageRules
@@ -39,16 +55,17 @@ var RcmsPTManager = RcmsPTManager || (function() {
     	  	,pValue;			//규칙코드
 
     	  for(var i=0;i<pValueArr.length;i++){
-    		  pValue	=	eval(prjRuleName+"."+pValueArr[i]);
+    		  pValue	=	eval(projectRuleName+"."+pValueArr[i]);
     		  
     		  switch(pValueArr[i].substr(0,1)){
     		  	//A타입 사용규칙 : 증빙구분에 따른 필수값 체크
 	      	  	case 'A' :
 		      	  	for(var prop in pValue.RequiredValue){
-		    			if(eval("frm."+pValue.RequiredValue[prop]) && (eval("frm."+pValue.RequiredValue[prop]+".value")==null || eval("frm."+pValue.RequiredValue[prop]+".value").replace(/ /gi,"")==""
-		    					// || eval("frm."+pValue.RequiredValue[prop]+".value")<=0
-		    					)){
-		    				alert("["+RcmsValueToName.getValuetoName("variables", pValue.RequiredValue[prop])+"]"+RcmsValueToName.getValuetoName("usageRules", pValueArr[i]));
+		      	  		//rule 조건이 true인경우
+		    			if(eval("frm."+pValue.RequiredValue[prop]) && (eval("frm."+pValue.RequiredValue[prop]+".value")==null || eval("frm."+pValue.RequiredValue[prop]+".value").replace(/ /gi,"")=="")){
+		    				//오류메시지
+		    				alert("["+TranslaterKeyValue.getValuetoName("variables", pValue.RequiredValue[prop])+"]"+TranslaterKeyValue.getValuetoName("usageRules", pValueArr[i]));
+		    				//focus
 		    				$('#'+pValue.RequiredValue[prop]).focus();
 		    				return false;
 		    			}
@@ -57,9 +74,12 @@ var RcmsPTManager = RcmsPTManager || (function() {
 		      	//F타입 사용규칙 : 값의 크기,범위 조건
 	      	  	case 'F' :
 		      	  	for(var prop in pValue.LTside){
-		      	  		console.log(eval("frm."+pValue.LTside[prop]+".value"));
+		      	  		//console.log(eval("frm."+pValue.LTside[prop]+".value"));
+		      	  		//rule 조건이 true인경우
 			      	   	if(eval("frm."+pValue.LTside[prop]) && (eval("frm."+pValue.LTside[prop]+".value" + pValue.sign + pValue.RTside) )){
-			      	   		alert("["+RcmsValueToName.getValuetoName("variables", pValue.LTside[prop])+"]"+RcmsValueToName.getValuetoName("usageRules",pValueArr[i]));
+		    				//오류메시지
+			      	   		alert("["+TranslaterKeyValue.getValuetoName("variables", pValue.LTside[prop])+"]"+TranslaterKeyValue.getValuetoName("usageRules",pValueArr[i]));
+			      	   		//focus
 			      	   		$('#'+pValue.LTside[prop]).focus();
 			      	   		return false;
 			      	  	}
